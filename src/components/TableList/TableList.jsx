567d997265f55ab102adde1df9d793a39ee3fc81
Jhon Giron <jhon.giron@woombatcg.com>
2024-11-14 20:05:16 -0500

    Creacion de la tabla

--- src/components/TableList/TableList.jsx
+++ src/components/TableList/TableList.jsx
@@ -1,9 +1,59 @@
-const TableList = () => {
-  return (
-    <div>
-        Desde TableList
-    </div>
-  )
-}
+import { useState, useEffect } from 'react'
+import API from '../../API.json'
 
-export default TableList
+const TableList = () => {
+
+    const [fuentes, setFuentes] = useState([])
+
+    useEffect(() => {
+        const getData = () =>  {
+            try {
+                setFuentes(API)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getData()
+    }, [])
+
+    console.log(fuentes.fuentes)
+
+  return (
+    <div className="table_list">
+      <div className="table-wrapper">
+        <table className="fl-table">
+          <thead>
+            <tr>
+              <th>Codigo</th>
+              <th>Nombre Fuente</th>
+              <th>Creada por</th>
+              <th>Editada por</th>
+              <th>Ultima Fecha de Modificacion</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                fuentes?.fuentes?.length > 0 ? (
+                    fuentes.fuentes.map(item => (
+                        <tr key={item.codigo}>
+                            <td>{item.codigo}</td>
+                            <td>{item.nombre_fuente}</td>
+                            <td>{item.creada_por}</td>
+                            <td>{item.modificada_por}</td>
+                            <td>{item.ultima_modificacion}</td>
+                            <td>
+                                <span><ion-icon name="create-outline"></ion-icon></span>
+                            </td>
+                        </tr>
+                    ))
+                ) : null
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TableList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar/Navbar'
 import NavUser from '../components/NavUser/NavUser'
+import TableList from '../components/TableList/TableList'
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <NavUser />
+      <TableList />
     </div>
   )
 }
